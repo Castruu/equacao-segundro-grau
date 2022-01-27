3151448722ead2666e3f4b432d026f8cedc634b0
BuildTools <unconfigured@null.spigotmc.org>
2022-01-27 17:18:28 -0300

    First Commit

---
+++ javascript/script.js
@@ -0,0 +1,27 @@
+function delta(a, b, c) {
+    return Math.pow(b, 2) - 4*a*c
+} 
+
+
+function bhaskara(a, b, c) {
+    var delt = delta(a, b, c);
+    if(parseFloat(delt) < 0) {
+        alert("Delta deve ser maior do que 0");
+        return null;
+    }
+    let x1 = (-b + Math.sqrt(delt))/(2 * a);
+    let x2 = (-b - Math.sqrt(delt))/(2 * a);
+    return [x1, x2]
+}
+
+
+
+function calcular() {
+    const a = document.getElementById("a").value;
+    const b = document.getElementById("b").value;
+    const c = document.getElementById("c").value;
+    let result = bhaskara(a, b, c);
+    document.getElementById("result").innerText = "X1 = " + result[0] + ", X2 = " + result [1];
+}
+
+
